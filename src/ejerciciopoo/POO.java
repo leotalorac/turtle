4839d2543fa8e6845772baf86a85bb43fd5474bc
lotalorafox <lotalora21@gmail.com>
2017-02-10 10:07:47 -0500

    ya posee los metodos para el cuadrado y el triangulo

---
+++ src/ejerciciopoo/POO.java
@@ -0,0 +1,61 @@
+package ejerciciopoo;
+
+import ch.aplu.turtle.Turtle;
+
+/**
+ *
+ * @author Equipo
+ */
+public class POO {
+   private  Turtle joe;
+   
+   public POO(){
+     this.joe =  new Turtle(); 
+   }
+   
+   public void dibujar(){
+       square(-100,-50,100,100);
+       square(-50,-50,100,100);
+       square(-100,0,50,50);
+       joe.right(180);
+       triangle(100,100,50);
+   } 
+   
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       POO e = new POO();
+       e.dibujar();
+    }
+
+
+    public void square(double a, double b, int c, int d){
+        joe.setX(a);
+        joe.setY(b);
+        for(int i=0 ;i<2;i++){
+            joe.right(90);
+            joe.fd(c);
+            joe.right(90);
+            joe.fd(d);
+        }
+    }
+
+    public void triangle(double a, double b,int c){
+        joe.setX(a);
+        joe.setY(b);
+        //triangle
+            joe.right(30);
+            joe.fd(c);
+            joe.right(120);
+            joe.fd(c);
+            joe.right(120);
+            joe.fd(c);
+
+
+    }
+
+
+
+    
+}
